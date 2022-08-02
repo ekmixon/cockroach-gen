b2bfdba8ca7bae49f4d9ed85ceca0d527ff118f6
Sourcery AI <>
2022-08-02 18:52:33 +0000

    'Refactored by Sourcery'

--- pkg/cli/interactive_tests/netcat.py
+++ pkg/cli/interactive_tests/netcat.py
@@ -5,12 +5,10 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(("0.0.0.0", 26257))
 server.listen(1)
-print "ready"
+import socket
 client_socket, addr = server.accept()
-print "connected"
-
+import socket
 while True:
-    c = client_socket.recv(1)
-    if c:
+    if c := client_socket.recv(1):
         sys.stdout.write("%c" % c)
         sys.stdout.flush()
